c10b5f651f20007ff14bb5260644d960b819eb4e
tibrook <guillouet.alexandre35@gmail.com>
2024-05-30 18:32:42 +0200

    [backend][Unit Test] Improve UT by using @ExtendWith(MockitoExtension.class)

--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServicesTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServicesTest.java
@@ -1,21 +1,21 @@
 package com.openclassrooms.starterjwt.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+@ExtendWith(MockitoExtension.class)
 public class UserServicesTest {
 
     @Mock
@@ -24,34 +24,40 @@ public class UserServicesTest {
     @InjectMocks
     private UserService userService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Nested
+    class UserDeletion {
+
+        @Test
+        void shouldDeleteUser() {
+            Long userId = 1L;
+            userService.delete(userId);
+            verify(userRepository).deleteById(userId);
+        }
     }
 
-    @Test
-    void testDeleteUser() {
-        Long userId = 1L;
-        userService.delete(userId);
-        verify(userRepository).deleteById(userId);
-    }
+    @Nested
+    class UserRetrieval {
 
-    @Test
-    void testFindByIdUserExists() {
-        Long userId = 1L;
-        User user = new User();
-        user.setId(userId);
-        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        @Test
+        void shouldReturnUserWhenExists() {
+            Long userId = 1L;
+            User user = new User();
+            user.setId(userId);
+            when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
 
-        User foundUser = userService.findById(userId);
-        assertEquals(userId, foundUser.getId());
-    }
+            User foundUser = userService.findById(userId);
+            
+            assertNotNull(foundUser);
+            assertEquals(userId, foundUser.getId());
+        }
 
-    @Test
-    void testFindByIdUserNotExists() {
-        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        @Test
+        void shouldReturnNullWhenUserNotExists() {
+            when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
 
-        User foundUser = userService.findById(1L);
-        assertNull(foundUser);
+            User foundUser = userService.findById(1L);
+            
+            assertNull(foundUser);
+        }
     }
 }
