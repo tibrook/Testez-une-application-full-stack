a2662411da51884419ea09b95e8767081b8743f6
tibrook <guillouet.alexandre35@gmail.com>
2024-06-05 06:58:33 +0200

    [Frontend][Unit Tests] Add Teacher unit tests

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,22 +1,64 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TeacherService } from './teacher.service';
+import { Teacher } from '../interfaces/teacher.interface';
 
 describe('TeacherService', () => {
   let service: TeacherService;
-
+  let httpTestingController: HttpTestingController;
+  let sampleTeacher1 =  {
+    id: 1,
+    firstName: 'Test',
+    lastName: 'User',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }; 
+  let sampleTeacher2 =  {
+    id: 2,
+    firstName: 'Test2',
+    lastName: 'User2',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }; 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [TeacherService]
     });
     service = TestBed.inject(TeacherService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  // Check if there is no outstanding request
+  afterEach(() => {
+    httpTestingController.verify(); 
+  });
+
+  it('should retrieve all teachers', () => {
+      const mockTeachers: Teacher[] = [
+        sampleTeacher1,
+        sampleTeacher2
+    ];
+
+    service.all().subscribe(teachers => {
+      expect(teachers.length).toBe(2);
+      expect(teachers).toEqual(mockTeachers);
+    });
+
+    const req = httpTestingController.expectOne('api/teacher');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTeachers);
+  });
+
+  it('should retrieve details of a specific teacher', () => {
+    const mockTeacher: Teacher = sampleTeacher1;
+
+    service.detail('1').subscribe(teacher => {
+      expect(teacher).toEqual(mockTeacher);
+    });
+
+    const req = httpTestingController.expectOne('api/teacher/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTeacher);
   });
 });
